418c66825028b707998151d999aa9a654b124cfc
mahfuj <mahfujr403@gmail.com>
2023-11-15 01:27:50 +0600

    cart_completed

--- cart.js
+++ cart.js
@@ -1,3 +1,4 @@
+// Event listener for the "Remove" buttons
 var product_id = document.getElementsByClassName("dlt");
 for (var i = 0; i < product_id.length; i++) {
   product_id[i].addEventListener("click", function (event) {
@@ -21,6 +22,7 @@ for (var i = 0; i < product_id.length; i++) {
   });
 }
 
+// Function to update the subtotal for a product
 function updateSubtotal(inputField) {
   console.log("Updating subtotal for product:", inputField.dataset.productId);
 
@@ -41,36 +43,12 @@ function updateSubtotal(inputField) {
   const subtotalElement = document.getElementById(`subtotal-${productId}`);
   subtotalElement.textContent = `৳${subtotal}`;
 
-  // Calculate and update the total cart subtotal
-  updateCartSubtotal();
+  // Calculate and update the total cart subtotal and discount
+  updateCartSubtotalAndDiscount();
 }
 
-function updateCartSubtotal() {
-  // Get all the subtotal elements
-  const subtotalElements = document.querySelectorAll('[id^="subtotal-"]');
-
-  // Initialize total cart subtotal
-  let totalCartSubtotal = 0;
-
-  // Iterate through each subtotal element and sum up the subtotals
-  subtotalElements.forEach((subtotalElement) => {
-    totalCartSubtotal += parseFloat(
-      subtotalElement.textContent.replace("৳", "")
-    );
-  });
-
-  // Update the total cart subtotal element
-  const totalCartSubtotalElement = document.getElementById("cart-subtotal");
-  totalCartSubtotalElement.textContent = `৳${totalCartSubtotal}`;
-}
-
-// Call the updateCartSubtotal function on page load
-updateCartSubtotal();
-
-
-
-
-function updateDiscount() {
+// Function to update the total cart subtotal and discount
+function updateCartSubtotalAndDiscount() {
   // Get all the subtotal elements
   const subtotalElements = document.querySelectorAll('[id^="subtotal-"]');
 
@@ -91,26 +69,40 @@ function updateDiscount() {
     totalCartSubtotal
   );
 
-  // Update the discount element.
-   const updtElement = document.getElementById("capp");
-   updtElement.textContent = `Coupoun Discount  (applied)`;
+  // Update the Cart Subtotal element
+  const cartSubtotalElement = document.getElementById("cart-subtotal");
+  cartSubtotalElement.textContent = `৳${totalCartSubtotal}`;
 
+  // Update the discount element
+   
   const discountElement = document.getElementById("discount");
   discountElement.textContent = `৳${couponDiscount}`;
+
+  // Calculate and update the total by subtracting the coupon discount
+  const total = totalCartSubtotal - couponDiscount;
+  const totalElement = document.getElementById("totalpay");
+  totalElement.textContent = `৳${total}`;
 }
 
+// Function to apply the coupon and update the total
 function applyCoupon() {
-  // Call updateCartSubtotal to apply the coupon and update the discount
-  updateDiscount();
+  // Call updateCartSubtotalAndDiscount to apply the coupon and update the total
+  updateCartSubtotalAndDiscount();
 }
 
+// Function to calculate the coupon discount
 function calculateCouponDiscount(couponCode, totalCartSubtotal) {
   // Check if the coupon code is "mahfuj"
   if (couponCode === "mahfuj") {
     // Assuming a fixed discount for the "mahfuj" coupon code
+    const appliedElement = document.getElementById("capp");
+    appliedElement.textContent = `Coupoun Discount (applied)`;
     return 50; // Assuming a fixed discount of 50৳
   }
+  const appliedElement = document.getElementById("capp");
+  appliedElement.textContent = `Coupoun Discount (not applied)`;
   return 0; // No discount if the coupon code is not "mahfuj"
 }
 
-
+// Call the updateCartSubtotalAndDiscount function on page load
+updateCartSubtotalAndDiscount();
